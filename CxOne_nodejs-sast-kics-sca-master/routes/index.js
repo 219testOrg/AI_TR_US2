7ed610c116fc21f9d73d9ae48bd740cfd8d6a8c5
Andrii Radishevskyi <191871656+cx-andrii-radishevskyi@users.noreply.github.com>
2026-03-17 14:26:25 +0200

    Checkmarx Bot - Auto Pull Request

--- CxOne_nodejs-sast-kics-sca-master/routes/index.js
+++ CxOne_nodejs-sast-kics-sca-master/routes/index.js
@@ -9,6 +9,7 @@ var streamBuffers = require('stream-buffers');
 var readline = require('readline');
 var moment = require('moment');
 var exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
 var validator = require('validator');
 
 // zip-slip
@@ -156,14 +157,23 @@ exports.create = function (req, res, next) {
   var imgRegex = /\!\[alt text\]\((http.*)\s\".*/;
   if (typeof (item) == 'string' && item.match(imgRegex)) {
     var url = item.match(imgRegex)[1];
-    console.log('found img: ' + url);
 
-    exec('identify ' + url, function (err, stdout, stderr) {
-      console.log(err);
-      if (err !== null) {
-        console.log('Error (' + err + '):' + stderr);
-      }
-    });
+    // Validate URL to prevent command injection
+    // Only allow http/https URLs and use execFile for safe execution
+    if (validator.isURL(url, { protocols: ['http', 'https'], require_protocol: true })) {
+      console.log('found img: ' + url);
+
+      // Use execFile instead of exec to prevent command injection
+      // execFile does not spawn a shell and treats arguments safely
+      execFile('identify', [url], function (err, stdout, stderr) {
+        console.log(err);
+        if (err !== null) {
+          console.log('Error (' + err + '):' + stderr);
+        }
+      });
+    } else {
+      console.log('Invalid URL format, skipping image identification');
+    }
 
   } else {
     item = parse(item);
